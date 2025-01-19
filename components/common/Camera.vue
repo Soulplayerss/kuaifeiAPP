8f5b1d20624fcf18e34add0a65b1c7aa0f721a45
Murdoch <1756140995@qq.com>
2025-01-19 23:28:34 +0800

    添加摄像头

---
+++ components/common/Camera.vue
@@ -0,0 +1,83 @@
+<template>
+	<view class="camera">
+		摄像头
+	</view>
+</template>
+
+<script>
+	import {
+		generateTime,
+		fnGetSignature
+	} from '@/assets/js/getSignature.js';
+	export default {
+		data(){
+			return {
+				headerParms:{
+					'Content-Type': 'application/json',
+					'uuid': '6763c5921f96978377583672',
+					'appKey': 'd2b66ca4998f1bcedbff6dd62c87c622',
+					'timeMillis': generateTime,
+					'signature': fnGetSignature
+				}
+			}
+		},
+		onLoad(options) {
+
+		},
+		methods: {
+			getDeviceToken() {
+				uni.request({
+					url: `https://api.jftechws.com/gwp/v3/rtc/device/token`,
+					method: 'POST',
+					header: this.headerParms,
+					data: {
+						sns: ['09bf5ddf079e8069']
+					},
+					success: (res) => {
+						this.getlivestream(res.data.data[0].token)
+					},
+					fail: (err) => {
+						uni.showToast({
+							title: '请求失败',
+							type: 'error',
+							icon: 'error',
+						});
+					},
+				})
+			},
+			getlivestream(deviceToken) {
+				uni.request({
+					url: `https://api.jftechws.com/gwp/v3/rtc/device/livestream/${deviceToken}`,
+					method: 'POST',
+					header: this.headerParms,
+					data: {
+						channel: '1',
+						stream: '1',
+						protocol: 'mp4-url',
+						username: 'admin'
+					},
+					success: (res) => {
+						console.log(res)
+					},
+					fail: (err) => {
+						uni.showToast({
+							title: '请求失败',
+							type: 'error',
+							icon: 'error',
+						});
+					},
+				})
+			}
+		},
+		mounted() {
+			this.getDeviceToken()
+		}
+	}
+</script>
+
+<style scoped lang="less">
+	.camera {
+		width: 100%;
+		height: 100%;
+	}
+</style>
